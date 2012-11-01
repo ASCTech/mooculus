95cc6549610a3d8dbdf120928404e039e5454817
Jim Fowler <fowler@math.osu.edu>
2012-11-01 11:18:02 -0400

    added rails app for assessment

---
+++ assessor/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assessor::Application.config.secret_token = 'd162137b98aaf7b3b0c3c0404c068534ba6d3e37b70848b3b0ac36fd2e6f6a3009ccb7cf431884c9ace4b2240a923a792f42b76ba7c9d767a0c267a6abbeb38d'
