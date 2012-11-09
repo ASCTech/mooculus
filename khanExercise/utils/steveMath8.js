777b4b338ccb3e28360d5bf0ef7861f3064a65f9
Steven Gubkin <sag17@case.edu>
2012-11-09 17:20:04 -0500

    More exercises

---
+++ khanExercise/utils/steveMath8.js
@@ -0,0 +1,167 @@
+$.extend(KhanUtil, {
+    
+    //takes num and returns +1 if num>0 or -1 if num<0
+    steveSign: function(num){
+        return num && num/Math.abs(num)
+    },
+    
+    // Approximates a root of f on the interval (xmin,xmax) by successively halving the interval.
+    steveRoot: function(f,xmin,xmax){
+        var l = xmin
+        var r = xmax
+        var z = 0
+        for (var i=0;i<10;i++){
+            z = (l + r)/2
+            if (KhanUtil.steveSign(f(l)) == KhanUtil.steveSign(f(z))){ l = z}
+            else{r = z}   
+          
+        }
+        return z
+    },
+    
+    goober: function(n,f){
+    	var list = []
+        for (var i=0;i<10;i++){
+            if (i>n){
+                list.push(f(i))
+            }
+        }
+       
+        return list
+        
+    },
+    
+    //takes a function and a list of abscissas, and returns an array of zeros - one zero between each pair of abscissas that are of
+    //opposite sign
+    locateZeros: function(f,abscissas){
+        var len = abscissas.length
+        var list = []
+        var z = 0
+        
+        for(var i=0;i<len-1;i++){
+           var x0 = abscissas[i]
+           var x1 = abscissas[i+1]
+           var y0 = f(x0)
+           var y1 = f(x1)
+           
+           if (KhanUtil.steveSign(y0) !== KhanUtil.steveSign(y1)){
+               z = KhanUtil.steveRoot(f,x0,x1)
+               list.push(KhanUtil.roundTo(1,z))
+           }
+         
+        }
+        return list
+    },
+    
+        steveCubic: function(x){return -Math.pow(x,3)/2+3*x/2},
+    
+    //niceFunction is a C^1 function which connects the points in "points".  It is designed to be used 
+    //in my "curveSketchingIntuition" exercise.  Every point in the "points" will have 0 slope, except the first and last point.
+    niceFunction: function(x,points){
+        
+        var len = points.length
+        
+        var x1 = points[0][0]
+        var x2 = points[1][0]
+        var y1 = points[0][1]
+        var y2 = points[1][1]
+        var k = (y1 - y2)/Math.pow(x1-x2,2)
+        
+        if (x<x2){return k*Math.pow(x-x2,2)+y2}
+        
+        for (var i=1;i<len-2;i++){
+            var x1 = points[i][0]
+            var x2 = points[i+1][0]
+            var y1 = points[i][1]
+            var y2 = points[i+1][1]
+            
+            xNew = (x-x1)*2/(x2-x1)-1
+            yNew = (KhanUtil.steveCubic(xNew)+1)*(y2-y1)/2+y1
+            if (x>=x1 && x<x2){return yNew}
+           
+            }
+        
+        
+        var x1 = points[len-2][0]
+        var x2 = points[len-1][0]
+        var y1 = points[len-2][1]
+        var y2 = points[len-1][1]
+        var k = (y2 - y1)/Math.pow(x1-x2,2)
+        if (x>=x1){return k*Math.pow(x-x1,2)+y1}
+        
+    },
+    
+   makeXList: function(){
+    var array = [-10]
+    var i=0
+    while(array[i]<10){
+    	x = array[i]+3*KhanUtil.randRange(1,3)
+    	if (x<10){array.push(x)}
+    	i=i+1
+        }
+	array.push(10)
+	return array
+    
+    },
+    
+    makeYList:function(min,max,n){
+        var excluded = [0]
+        var array = [KhanUtil.randRangeExclude(min,max,excluded)]
+        excluded.push(array[0])
+        array.push[KhanUtil.randRangeExclude(min,max,excluded)]
+        excluded = [0]
+        for (var i=1;i<n;i++){
+            if (array[i-2]<array[i-1]){
+                array.push(KhanUtil.randRangeExclude(min,array[i-1]-1,excluded))
+                }
+            else{array.push(KhanUtil.randRangeExclude(array[i-1]+1,max,excluded))}
+            }
+        
+        return array
+    
+    },
+    
+        makeCoordinates:  function(array1,array2){
+        var array = []
+        for (var i=0;i<array1.length;i++){
+            array.push([array1[i],array2[i]])
+        }
+        return array
+    },
+    
+    
+    //numerically integrates a function f over the interval [a,b]
+	numInt: function(f,a,b){
+		var delta = .01
+		var sum = 0
+		var i = 0
+		if (a<=b){
+		while ((a+delta*i)<=b)
+		
+			{sum = sum+delta*f(a+i*delta)
+			 i++
+			}
+		return sum}
+		if (a>b){return -KhanUtil.numInt(f,b,a)}
+		},
+    
+    
+    //////////////////////////
+
+    //The following two functions were used in previous prototypes of my 
+    //exercises, but I have not used them.  Maybe they are useful to someone.
+    
+
+    
+    
+    // an implementation of Newton's method. Takes a function and an approximate zero, returns a better approximation.
+    newtonRoot: function(f,a){
+        var z = a
+        var m = (f(z+.01)-f(z-.01))/.02
+        for(var i=0;i<2;i++){
+            z = z-f(z)/m
+            m = (f(z+.01)-f(z-.01))/.02
+        }
+        return z
+    },
+});
