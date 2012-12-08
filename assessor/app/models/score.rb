3e6bbd25cc6a6473d0533c0b4633ea63992ff113
Ryan Kowalick <rkowalick@gmail.com>
2012-12-08 10:29:49 -0500

    Added Factory Girl For Testing, Modified Ex Views
    Factory girl gem was added for helping with unit tests.
    
    Modified the exercise index view to allow a more "personalized"
    listing of exercises.

--- assessor/app/models/score.rb
+++ assessor/app/models/score.rb
@@ -1,5 +1,6 @@
 class Score < ActiveRecord::Base
-  attr_accessible :time_taken, :attempt_number, :complete, :count_hints, :seed
+  attr_accessible :time_taken, :attempt_number, :complete, :count_hints, :seed,
+    :attempt_content
   belongs_to :exercise
   belongs_to :user
 end
--- assessor/test/functional/home_controller_test.rb
+++ assessor/test/functional/home_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
   test "should get index" do
     get :index
     assert_response :success
