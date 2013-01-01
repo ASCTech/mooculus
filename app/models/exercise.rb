729c9e7556798f01b525d4224ba872fec5f29c73
Jim Fowler <fowler@math.osu.edu>
2013-01-01 11:29:46 -0500

    Can pick problem randomly from the bag

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,4 +2,19 @@ class Exercise < ActiveRecord::Base
   attr_accessible :title, :description, :page, :problem_number
   has_many :scores, :dependent => :destroy
   has_many :problems, :dependent => :destroy
+
+  def total_weight
+    problems.sum(:weight)
+  end
+
+  def problem_from_bag
+    index = rand(self.total_weight)
+    for problem in problems do
+      index = index - problem.weight
+      if index < 0 
+        return problem
+      end
+    end
+    return nil
+  end
 end
