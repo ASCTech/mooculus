0bd7771878323ad95fec95a8619aaddc3897b6ee
Jim Fowler <fowler@math.osu.edu>
2013-01-01 14:23:07 -0500

    Routes for handouts and videos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Assessor::Application.routes.draw do
+  resources :handouts
+
+  resources :videos
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
