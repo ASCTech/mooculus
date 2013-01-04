48cd3a88a2dcc09fd73970f6c714c1a894cc4044
Jim Fowler <fowler@math.osu.edu>
2013-01-04 14:47:14 -0500

    migrated badges

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_merit
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
