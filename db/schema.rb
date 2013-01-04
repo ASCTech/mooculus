bf57b1162f5fb2c9caeeedc681889110a2c3b8b6
Ryan Kowalick <rkowalick@gmail.com>
2013-01-03 22:54:01 -0500

    Added order attribute to exercises

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130102001138) do
+ActiveRecord::Schema.define(:version => 20130104034614) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20130102001138) do
     t.string   "page"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "order"
   end
 
   create_table "handouts", :force => true do |t|
