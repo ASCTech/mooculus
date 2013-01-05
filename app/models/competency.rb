2b67e3bfdbe0e34a08ea4be5505fac8e2c0a483c
Jim Fowler <fowler@math.osu.edu>
2013-01-05 00:39:45 -0500

    Fixing additional spelling errors in competency

--- app/models/competency.rb
+++ app/models/competency.rb
@@ -81,7 +81,7 @@ class Competency < ActiveRecord::Base
     @competency = Competency.where(conditions).limit(1).first || Competency.create(conditions)
 
     if not @competency.estimate.nil?
-      @comptency.uncertainty = [ (p - @competency.estimate).abs, 1.0 - p, p ].min
+      @competency.uncertainty = [ (p - @competency.estimate).abs, 1.0 - p, p ].min
     end
 
     if @competency.min_estimate.nil? or (@competency.min_estimate > p)
