e34d48276ee75826eff785df205fa229a58703eb
Ryan Kowalick <rkowalick@gmail.com>
2013-01-05 00:05:54 -0500

    Added Progress Bars, fixed css, fixed profile issues

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,22 +1,25 @@
 module ProfileHelper
   def genders
-    options_for_select([["", ""], %w(Female Female), %w(Male Male)], @user.gender)
+    default = @user.gender || ""
+    options_for_select([["", ""], %w(Female Female), %w(Male Male)], default)
   end
 
   def months
+    default = @user.birthday? ? @user.birthday.month : 1
     options_for_select({ "Jan" => 1, "Feb" => 2, "Mar" => 3, "Apr" => 4,
                        "May" => 5, "Jun" => 6, "Jul" => 7, "Aug" => 8,
                        "Sep" => 9, "Oct" => 10, "Nov" => 11, "Dec" => 12 },
-                      @user.birthday.month)
+                      default)
   end
 
   def days
-    options_for_select((1..31).map { |i| [i, i] }, @user.birthday.day)
+    default = @user.birthday? ? @user.birthday.day : 1
+    options_for_select((1..31).map { |i| [i, i] }, default)
   end
 
   
   def years
-    options_for_select((1900..2010).map { |i| [i, i] }.reverse, 
-                       @user.birthday.year)
+    default = @user.birthday? ? @user.birthday.year : 2010 
+    options_for_select((1900..2010).map { |i| [i, i] }.reverse, default)
   end
 end
