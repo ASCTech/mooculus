19a91ec3a349f653df40c0dfacb67f6f479cdf32
Ryan Kowalick <rkowalick@gmail.com>
2013-01-05 01:45:09 -0500

    Added functioning progress bars to index and progress views

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,2 +1,13 @@
 module ExercisesHelper
+  def progress_on(exercise)
+    competency = @competencies.find_by_exercise_id(exercise.id)
+    progress = (competency.max_estimate * 100).round
+    modifier = case progress
+               when 0..50 then " bar-danger"
+               when 51..80 then " bar-warning"
+               else " bar-success"
+               end
+
+    %(<div class="bar#{modifier}" style="width: #{progress}%;"></div>)
+  end
 end
