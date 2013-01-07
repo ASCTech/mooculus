98ff794200fc22ace01af009b41c19a675af4c7a
Jim Fowler <fowler@math.ohio-state.edu>
2013-01-07 03:15:02 -0500

    Cannot get a rails console on production sigh

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,13 +69,3 @@ task :tail, :roles => :app do
   end
 end
 
-desc "Remote console on the production appserver"
-task :console, :roles => :app do
-  input = ''
-  run "cd #{current_path} && ./script/console production" do
-    |channel, stream, data|
-    next if data.chomp == input.chomp || data.chomp == ''
-    print data
-    channel.send_data(input = $stdin.gets) if data =~ /^(>|\?)>/
-  end
-end
