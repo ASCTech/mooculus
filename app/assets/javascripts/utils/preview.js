5e04f55b71f41aac30e20888f707842b2e9c7cfc
Jim Fowler <fowler@math.osu.edu>
2013-02-07 17:33:56 -0500

    Accept either answer type

--- app/assets/javascripts/utils/preview.js
+++ app/assets/javascripts/utils/preview.js
@@ -1,28 +1,36 @@
 $(function() {
     $(document).keyup( function() {
 	$( "div.parsed-expression-answer-type" ).each(function() {
-	    var mathquill_box = $(this);
+	    var input_box = $(this);
 
 	    window.setTimeout( function() {
-		var text = $(mathquill_box).children('.mathquill-editable').mathquill('latex');
-		console.log( text );
+		var text; 
+		var parser; 
+
+		if ($(input_box).children('.mathquill-editable').length > 0) {
+		    text = $(input_box).children('.mathquill-editable').mathquill('latex');
+		    parser = MathFunction.parse_tex;
+		} else {
+		    text = $(input_box).children('.parsed-expression').val();
+		    parser = MathFunction.parse;
+		}
 
 		if (text.length > 0) {
-		    var math = MathJax.Hub.getAllJax($(mathquill_box).children(".MathPreview")[0])[0];
+		    var math = MathJax.Hub.getAllJax($(input_box).children(".MathPreview")[0])[0];
 		    try
 		    {
-			var latex = MathFunction.parse_tex(text).tex();
+			var latex = parser(text).tex();
 			MathJax.Hub.Queue(["Text",math,latex]);
-			$(mathquill_box).children(".MathFunctionError").hide();
-			$(mathquill_box).children(".MathPreview").show();
+			$(input_box).children(".MathFunctionError").hide();
+			$(input_box).children(".MathPreview").show();
 		    }
 		    catch(err)
 		    {
-			$(mathquill_box).children(".MathFunctionError").show();
-			$(mathquill_box).children(".MathPreview").hide();
+			$(input_box).children(".MathFunctionError").show();
+			$(input_box).children(".MathPreview").hide();
 		    }
 		} else {
-		    $(mathquill_box).children(".MathPreview").hide();
+		    $(input_box).children(".MathPreview").hide();
 		}
 	    }, 250 );
 	});
