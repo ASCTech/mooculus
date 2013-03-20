1c3c9458017c0dfcabf8415f3591446e92554d23
mikegee <gee.24@osu.edu>
2013-03-20 13:36:01 -0400

    ActionMailer's SMTP conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,15 @@ Assessor::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'exchange.asc.ohio-state.edu',
+    :port => 25,
+    :enable_starttls_auto => false
+  }
+  config.action_mailer.default_url_options = {
+    :host => 'mooculus.osu.edu',
+  }
+
 end
