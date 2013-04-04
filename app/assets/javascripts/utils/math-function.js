c39979b872210efeae6af69abd5fc78360d8163c
Jim Fowler <fowler@math.osu.edu>
2013-04-04 13:27:37 -0400

    Integrating against arbitrary variable now

--- app/assets/javascripts/utils/math-function.js
+++ app/assets/javascripts/utils/math-function.js
@@ -956,10 +956,12 @@ var MathFunction = (function () {
 	integrate: function(x,a,b) {
 	    var intervals = 100;
 	    var total = 0.0;
+	    var bindings = new Object();
 
             for( var i=0; i < intervals; i++ ) {
 		var sample_point = a + ((b - a) * (i + 0.5) / intervals);
-		total = total + this.evaluate( { x : sample_point } );
+		bindings[x] = sample_point;
+		total = total + this.evaluate( bindings );
 	    }
 
 	    return total * (b - a) / intervals;
