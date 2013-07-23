f2ae6946d401f0c9576d9454f8075077bde1c80c
Jim Fowler <fowler@math.osu.edu>
2013-07-23 11:49:49 -0400

    Progress bars now become striped when they are above 95 percent

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -8,6 +8,10 @@ module ExercisesHelper
                else " bar-success"
                end
 
-    %(<div class="bar#{modifier}" style="width: #{progress}%;"></div>)
+    if (progress > 0.95)
+      return %(<div class="progress progress-striped" style="margin-bottom: 0px;"><div class="bar bar-success" style="width: 100%;"></div></div>)
+    else
+      return %(<div class="progress" style="margin-bottom: 0px;"><div class="bar#{modifier}" style="width: #{progress}%;"></div></div>)
+    end
   end
 end
