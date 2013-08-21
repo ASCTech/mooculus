5bf2fc749bca0a30dc68dc468869d3c915a63600
Jim Fowler <fowler@math.osu.edu>
2013-08-21 15:35:11 -0400

    Correctly including parentheses for towers of exponentials regardless

--- app/assets/javascripts/utils/ast-to-text.js
+++ app/assets/javascripts/utils/ast-to-text.js
@@ -107,7 +107,7 @@ var astToText = (function(){
 	    if (operands[0][0] === "^")
 		return "(" + factor(operands[0]) + ")^" + factor(operands[1]);
 	    if (operands[1][0] === "^")
-		return factor(operands[0]) + "^(" + factor(operands[1]) ")";
+		return factor(operands[0]) + "^(" + factor(operands[1]) + ")";
 
 	    if (operands[0][0] === "sin")
 		return "sin^" + factor(operands[1]) + " " + factor(operands[0][1]);
